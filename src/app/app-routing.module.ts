d9e10834e7af779d79ea04645852ea37a0af78f8
Adan Smith Duque Aquino <smith.asda87@gmail.com>
2025-07-02 19:14:59 -0500

    actualizacion 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  // Puedes agregar más rutas como productos, carrito, etc.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
